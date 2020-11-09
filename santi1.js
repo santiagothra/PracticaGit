7303433a36853b09f4985706bf429b0b8b4998a4
santiagothra <santiiagothomas@gmail.com>
2020-11-09 12:58:52 -0300

    borre variable lunes

--- santi1.js
+++ santi1.js
@@ -1,5 +1,4 @@
 // buenos dias 
 let variable = "los lunes no son buenos dias";
 let varia = "ya quiero que sea viernes";
-let lunes = "mañana es martes";
 let martes = "sigo queriendo que sea viernes";

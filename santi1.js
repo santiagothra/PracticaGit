26acbe1980c5f52ef1b43ff7277973797a2506ed
Sebastian Krawiec <sebastian.arielk@gmail.com>
2020-11-09 12:56:58 -0300

    agregando variable martes

--- santi1.js
+++ santi1.js
@@ -1,4 +1,5 @@
 // buenos dias 
 let variable = "los lunes no son buenos dias";
 let varia = "ya quiero que sea viernes";
-let lunes = "mañana es martes"
+let lunes = "mañana es martes";
+let martes = "sigo queriendo que sea viernes";
